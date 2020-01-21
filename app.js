36737015eac4d61da715e8c1b08701701dc69429
Jamiebullock87 <jamiebullock1987@gmail.com>
2020-01-21 21:20:26 +0000

    headers

--- app.js
+++ app.js
@@ -17,10 +17,9 @@ require('dotenv').config()
 // }
 // app.use(cors(corsOptions));
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Credentials', true);
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header('Access-Control-Allow-Origin', 'http://jamiebullock.io');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Allow-Headers', 'Origin,Content-Type,Accept,content-type,application/json');
     next();
 });
 
